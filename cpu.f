ebc561eaa15fe8ac977fcc532c51c0abe8685486
zxc12523 <danzxc910624@gmail.com>
2022-12-31 15:28:41 +0800

    test

--- cpu.f
+++ cpu.f
@@ -2,24 +2,24 @@
 ./testbench.v
 
 // Your files
+./codes/alu.v
 ./codes/branch_unit.v
-./codes/cpu.v
 ./codes/control.v
+./codes/cpu.v
+./codes/data_memory.v
+./codes/EXE_buffer.v
+./codes/forward_unit.v
 ./codes/hazard_unit.v
 ./codes/ID_buffer.v
 ./codes/IF_buffer.v
 ./codes/imm_generator.v
-./codes/pc.v
-./codes/register.v
 ./codes/instruction_memory.v
-./codes/data_memory.v
 ./codes/Mem_address.v
 ./codes/MEM1_buffer.v
 ./codes/MEM2_buffer.v
-./codes/EXE_buffer.v
-./codes/forward_unit.v
+./codes/multiplexer2.v
 ./codes/multiplexer4.v
 ./codes/multiplexer8.v
 ./codes/multiplexer32.v
-./codes/alu.v
-./codes/forward_unit.v
+./codes/pc.v
+./codes/register.v
